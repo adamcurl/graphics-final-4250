cad9b67b8f48c269def263b4c28d63c948188260
Adam Curl <adamcurl6@gmail.com>
2018-11-16 15:19:06 -0600

    finishes part 1

--- proj4.js
+++ proj4.js
@@ -1,3 +1,4 @@
+// Project 4 by Adam Curl
 var canvas;
 var gl;
 var program;
@@ -15,7 +16,7 @@ var projectionMatrixLoc;
 var modelViewStack = [];
 
 // Light
-var lightPosition = vec4(1.8, 1, 2, 0.0);
+var lightPosition = vec4(4, 5, 2, 0.0);
 var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0);
 var lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
 var lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
@@ -30,18 +31,21 @@ var materialShininess = 50.0;
 var ctm;
 var ambientColor, diffuseColor, specularColor;
 
-// to keep track of point positions
+// bow point positions
 var bowStartPoint = 0;
 var bowEndPoint = 0;
+
+// to keep track of point positions
 var boxPointEnd = 0;
 var bowPointEnd = 0;
 var wrapPointEnd = 0;
+var prismPointEnd = 0;
 
 //	"camera" variables
-var zoomFactor = 3.0;
-var translateFactorX = -0.75;
-var translateFactorY = -0.25;
-var phi = 30; // camera rotating angles
+var zoomFactor = 5.0;
+var translateFactorX = -1.3;
+var translateFactorY = 0;
+var phi = 60; // camera rotating angles
 var theta = 20;
 var Radius = 1.5; // radius of the camera
 
@@ -61,17 +65,16 @@ var deg = 5;
 
 // vertices
 var vertices = [
-  // box points
-  vec4(0, 0, 0, 1), // A(0)
-  vec4(1, 0, 0, 1), // B(1)
-  vec4(1, 1, 0, 1), // C(2)
-  vec4(0.5, 1.5, 0, 1), // D(3)
-  vec4(0, 1, 0, 1), // E(4)
-  vec4(0, 0, 1, 1), // F(5)
-  vec4(1, 0, 1, 1), // G(6)
-  vec4(1, 1, 1, 1), // H(7)
-  vec4(0.5, 1.5, 1, 1), // I(8)
-  vec4(0, 1, 1, 1), // J(9)
+  vec4(-0.5, -0.5, 0.5, 1.0),
+  vec4(-0.5, 0.5, 0.5, 1.0),
+  vec4(0.5, 0.5, 0.5, 1.0),
+  vec4(0.5, -0.5, 0.5, 1.0),
+  vec4(-0.5, -0.5, -0.5, 1.0),
+  vec4(-0.5, 0.5, -0.5, 1.0),
+  vec4(0.5, 0.5, -0.5, 1.0),
+  vec4(0.5, -0.5, -0.5, 1.0),
+  vec4(-0.5, 0, 0, 1), //8
+  vec4(0.5, 0, 0, 1), //9
   vec4(0.25, 1.01, 1.01, 1), // top-left-front 10
   vec4(0.25, -0.01, 1.01, 1), // btm-left-front 11
   vec4(0.75, -0.01, 1.01, 1), // btm-right-front 12
@@ -79,19 +82,16 @@ var vertices = [
   vec4(0.25, 1.01, -0.01, 1), // top-left-back 14
   vec4(0.75, 1.01, -0.01, 1), // top-right-back 15
   vec4(0.25, -0.01, -0.01, 1), // btm-left-back 16
-  vec4(0.75, -0.01, -0.01, 1) // btm-right-back 17
+  vec4(0.75, -0.01, -0.01, 1), // btm-right-back 17
+  vec4(0, 0.5, 0, 1), // 18
+  vec4(-0.5, 0, 1, 1), // 19
+  vec4(0.5, 0, 1, 1), // 20
+  vec4(0, 0.5, 1, 1) // 21
 ];
 
-function scale4(a, b, c) {
-  var result = mat4();
-  result[0][0] = a;
-  result[1][1] = b;
-  result[2][2] = c;
-  return result;
-}
-
+/********** GENERATE FUNCTIONS **********/
 function GenerateBowPoints() {
-  var Radius = 0.5;
+  var Radius = 0.4;
   var numPoints = 80;
   bowStartPoint = vertices.length;
   // generate first side of bow
@@ -111,6 +111,15 @@ function GenerateBowPoints() {
   bowEndPoint = vertices.length - 1;
 }
 
+/********** QUAD FUNCTIONS **********/
+function QuadPrism() {
+  quad(21, 20, 9, 18);
+  quad(18, 8, 19, 21);
+  quad(19, 8, 9, 20);
+  tri(21, 19, 20);
+  tri(18, 9, 8);
+  prismPointEnd = numVertices;
+}
 function QuadPresentBow() {
   for (var i = bowStartPoint; i < bowStartPoint + 79; i++) {
     var a = i;
@@ -122,19 +131,17 @@ function QuadPresentBow() {
   // connect bow
   quad(bowStartPoint, bowStartPoint + 80, bowEndPoint, bowStartPoint + 79);
   bowPointEnd = numVertices;
-  console.log("bowpointend", bowPointEnd);
 }
 
 // draw shapes
 function QuadPresentBox() {
-  quad(0, 5, 9, 4); // AFJE left side
-  quad(1, 2, 7, 6); // BCHG right side
-  quad(0, 1, 6, 5); // ABGF bottom
-  quad(5, 6, 7, 9); // FGHJ front
-  quad(0, 4, 2, 1); // AECB back
-  quad(4, 9, 7, 2); // FJHC top
+  quad(1, 0, 3, 2);
+  quad(2, 3, 7, 6);
+  quad(3, 0, 4, 7);
+  quad(6, 5, 1, 2);
+  quad(4, 5, 6, 7);
+  quad(5, 4, 0, 1);
   boxPointEnd = numVertices;
-  console.log("boxpointend", boxPointEnd);
 }
 
 function QuadPresentWrap() {
@@ -143,11 +150,82 @@ function QuadPresentWrap() {
   quad(15, 17, 16, 14);
   quad(11, 16, 17, 12);
   wrapPointEnd = numVertices;
-  console.log("wrappointend", wrapPointEnd);
+}
+
+/********** DRAW FUNCTIONS **********/
+function DrawSolidCube(length) {
+  modelViewStack.push(modelViewMatrix);
+  s = scale4(length, length, length); // scale to the given width/height/depth
+  modelViewMatrix = mult(modelViewMatrix, s);
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+
+  gl.drawArrays(gl.TRIANGLES, 0, boxPointEnd);
+
+  modelViewMatrix = modelViewStack.pop();
+}
+
+function DrawWall(thickness) {
+  var s, t;
+
+  // draw thin wall with top = xz-plane, corner at origin
+  modelViewStack.push(modelViewMatrix);
+
+  t = translate(0.5, 0.5 * thickness, 0.5);
+  s = scale4(1.0, thickness, 1.0);
+  modelViewMatrix = mult(mult(modelViewMatrix, t), s);
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+  DrawSolidCube(1);
+
+  modelViewMatrix = modelViewStack.pop();
 }
 
 function DrawPresentBox() {
-  // change color of object
+  gl.drawArrays(gl.TRIANGLES, 0, boxPointEnd);
+}
+
+function DrawPresentBow() {
+  // draw right side of bow
+  modelViewStack.push(modelViewMatrix);
+  modelViewMatrix = mult(modelViewMatrix, translate(0.3, 0.72, -0.2));
+  modelViewMatrix = mult(modelViewMatrix, scale4(0.8, 0.4, 1));
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+  gl.drawArrays(gl.TRIANGLES, boxPointEnd, bowPointEnd - boxPointEnd);
+  modelViewMatrix = modelViewStack.pop();
+  // draw left side of bow
+  modelViewStack.push(modelViewMatrix);
+  modelViewMatrix = mult(modelViewMatrix, translate(-0.3, 0.72, -0.2));
+  modelViewMatrix = mult(modelViewMatrix, scale4(0.8, 0.4, 1));
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+  gl.drawArrays(gl.TRIANGLES, boxPointEnd, bowPointEnd - boxPointEnd);
+  modelViewMatrix = modelViewStack.pop();
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+}
+
+function DrawPresentWrap() {
+  modelViewStack.push(modelViewMatrix);
+  modelViewMatrix = mult(modelViewMatrix, translate(-0.5, -0.5, -0.5));
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+  gl.drawArrays(gl.TRIANGLES, bowPointEnd, wrapPointEnd - bowPointEnd);
+  modelViewMatrix = modelViewStack.pop();
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+
+  // rotate and draw other wrap side
+  modelViewStack.push(modelViewMatrix);
+  modelViewMatrix = mult(modelViewMatrix, translate(0, 0, 0));
+  modelViewMatrix = mult(modelViewMatrix, rotate(90, 0, 1, 0));
+  modelViewMatrix = mult(modelViewMatrix, translate(-0.5, -0.5, -0.5));
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+  gl.drawArrays(gl.TRIANGLES, bowPointEnd, wrapPointEnd - bowPointEnd);
+  modelViewMatrix = modelViewStack.pop();
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+}
+
+function DrawPrism() {
+  gl.drawArrays(gl.TRIANGLES, wrapPointEnd, prismPointEnd - wrapPointEnd);
+}
+
+function DrawPresent() {
+  // red box
   materialAmbient = vec4(1, 0.1, 0.1, 1.0);
   materialDiffuse = vec4(1, 0.1, 0.1, 1.0);
   ambientProduct = mult(lightAmbient, materialAmbient);
@@ -160,11 +238,8 @@ function DrawPresentBox() {
     gl.getUniformLocation(program, "diffuseProduct"),
     flatten(diffuseProduct)
   );
-  gl.drawArrays(gl.TRIANGLES, 0, boxPointEnd);
-}
-
-function DrawPresentBow() {
-  // change color of object
+  DrawSolidCube(1);
+  // green bow and wrap
   materialAmbient = vec4(0.1, 0.7, 0.1, 1.0);
   materialDiffuse = vec4(0.1, 0.7, 0.1, 1.0);
   ambientProduct = mult(lightAmbient, materialAmbient);
@@ -177,26 +252,14 @@ function DrawPresentBow() {
     gl.getUniformLocation(program, "diffuseProduct"),
     flatten(diffuseProduct)
   );
-  // draw right side of bow
-  modelViewStack.push(modelViewMatrix);
-  modelViewMatrix = mult(modelViewMatrix, translate(0.9, 1.22, 0.3));
-  modelViewMatrix = mult(modelViewMatrix, scale4(0.8, 0.4, 1));
-  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
-  gl.drawArrays(gl.TRIANGLES, boxPointEnd, bowPointEnd - boxPointEnd);
-  modelViewMatrix = modelViewStack.pop();
-  // draw left side of bow
-  modelViewStack.push(modelViewMatrix);
-  modelViewMatrix = mult(modelViewMatrix, translate(0.1, 1.22, 0.3));
-  modelViewMatrix = mult(modelViewMatrix, scale4(0.8, 0.4, 1));
-  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
-  gl.drawArrays(gl.TRIANGLES, boxPointEnd, bowPointEnd - boxPointEnd);
-  modelViewMatrix = modelViewStack.pop();
-  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+  DrawPresentBow();
+  DrawPresentWrap();
 }
 
-function DrawPresentWrap() {
-  materialAmbient = vec4(0.1, 0.7, 0.1, 1.0);
-  materialDiffuse = vec4(0.1, 0.7, 0.1, 1.0);
+function DrawHouse() {
+  // draw house body
+  materialAmbient = vec4(1, 0.7, 0.5, 1.0);
+  materialDiffuse = vec4(1, 0.7, 0.5, 1.0);
   ambientProduct = mult(lightAmbient, materialAmbient);
   diffuseProduct = mult(lightDiffuse, materialDiffuse);
   gl.uniform4fv(
@@ -207,39 +270,121 @@ function DrawPresentWrap() {
     gl.getUniformLocation(program, "diffuseProduct"),
     flatten(diffuseProduct)
   );
-  gl.drawArrays(gl.TRIANGLES, bowPointEnd, wrapPointEnd - bowPointEnd);
-
   modelViewStack.push(modelViewMatrix);
-  modelViewMatrix = mult(modelViewMatrix, translate(0.5, 0.5, 0.5));
-  modelViewMatrix = mult(modelViewMatrix, rotate(90, 0, 1, 0));
-  modelViewMatrix = mult(modelViewMatrix, translate(-0.5, -0.5, -0.5));
-  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
-  gl.drawArrays(gl.TRIANGLES, bowPointEnd, wrapPointEnd - bowPointEnd);
+  modelViewMatrix = mult(modelViewMatrix, translate(2, 1.02, 3));
+  modelViewMatrix = mult(modelViewMatrix, scale4(2, 2, 3));
+  DrawSolidCube(1);
   modelViewMatrix = modelViewStack.pop();
+  modelViewStack.push(modelViewMatrix);
+  modelViewMatrix = mult(modelViewMatrix, translate(2, 2.02, 1.5));
+  modelViewMatrix = mult(modelViewMatrix, scale4(2, 2, 3));
   gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+  DrawPrism();
+  modelViewMatrix = modelViewStack.pop();
+
+  // draw roof
+  materialAmbient = vec4(1, 0, 0, 1.0);
+  materialDiffuse = vec4(1, 0, 0, 1.0);
+  ambientProduct = mult(lightAmbient, materialAmbient);
+  diffuseProduct = mult(lightDiffuse, materialDiffuse);
+  gl.uniform4fv(
+    gl.getUniformLocation(program, "ambientProduct"),
+    flatten(ambientProduct)
+  );
+  gl.uniform4fv(
+    gl.getUniformLocation(program, "diffuseProduct"),
+    flatten(diffuseProduct)
+  );
+  modelViewStack.push(modelViewMatrix);
+  modelViewMatrix = mult(modelViewMatrix, translate(1.3, 2.35, 3));
+  modelViewMatrix = mult(modelViewMatrix, rotate(45, 0, 0, 1));
+  modelViewMatrix = mult(modelViewMatrix, scale4(2.0, 0.02, 3.0));
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+  DrawSolidCube(1);
+  modelViewMatrix = modelViewStack.pop();
+  modelViewStack.push(modelViewMatrix);
+  modelViewMatrix = mult(modelViewMatrix, translate(2.7, 2.35, 3));
+  modelViewMatrix = mult(modelViewMatrix, rotate(-45, 0, 0, 1));
+  modelViewMatrix = mult(modelViewMatrix, scale4(2.0, 0.02, 3.0));
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+  DrawSolidCube(1);
+  modelViewMatrix = modelViewStack.pop();
+
+  // draw door
+  materialAmbient = vec4(0.6, 0.4, 0.2, 1.0);
+  materialDiffuse = vec4(0.6, 0.4, 0.2, 1.0);
+  ambientProduct = mult(lightAmbient, materialAmbient);
+  diffuseProduct = mult(lightDiffuse, materialDiffuse);
+  gl.uniform4fv(
+    gl.getUniformLocation(program, "ambientProduct"),
+    flatten(ambientProduct)
+  );
+  gl.uniform4fv(
+    gl.getUniformLocation(program, "diffuseProduct"),
+    flatten(diffuseProduct)
+  );
+  modelViewStack.push(modelViewMatrix);
+  modelViewMatrix = mult(modelViewMatrix, translate(1.5, 0.02, 4.55));
+  modelViewMatrix = mult(modelViewMatrix, rotate(-90, 1.0, 0.0, 0.0));
+  modelViewMatrix = mult(modelViewMatrix, scale4(1, 1, 2));
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+  DrawWall(0.02);
+  modelViewMatrix = modelViewStack.pop();
+
+  // draw window
+  materialAmbient = vec4(1, 1, 1, 1.0);
+  materialDiffuse = vec4(1, 1, 1, 1.0);
+  ambientProduct = mult(lightAmbient, materialAmbient);
+  diffuseProduct = mult(lightDiffuse, materialDiffuse);
+  gl.uniform4fv(
+    gl.getUniformLocation(program, "ambientProduct"),
+    flatten(ambientProduct)
+  );
+  gl.uniform4fv(
+    gl.getUniformLocation(program, "diffuseProduct"),
+    flatten(diffuseProduct)
+  );
+  modelViewStack.push(modelViewMatrix);
+  modelViewMatrix = mult(modelViewMatrix, translate(3.01, 0.5, 2));
+  modelViewMatrix = mult(modelViewMatrix, rotate(90.0, 0.0, 0.0, 1.0));
+  modelViewMatrix = mult(modelViewMatrix, scale4(1, 1, 2));
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+  DrawWall(0.02);
+  modelViewMatrix = modelViewStack.pop();
 }
 
-function quad(a, b, c, d) {
-  var t1 = subtract(vertices[b], vertices[a]);
-  var t2 = subtract(vertices[c], vertices[b]);
-  var normal = cross(t1, t2);
-  var normal = vec3(normal);
-  normal = normalize(normal);
+function DrawEnvironment() {
+  materialAmbient = vec4(0.1, 0.1, 0.6, 1.0);
+  materialDiffuse = vec4(0.1, 0.1, 0.6, 1.0);
+  ambientProduct = mult(lightAmbient, materialAmbient);
+  diffuseProduct = mult(lightDiffuse, materialDiffuse);
+  gl.uniform4fv(
+    gl.getUniformLocation(program, "ambientProduct"),
+    flatten(ambientProduct)
+  );
+  gl.uniform4fv(
+    gl.getUniformLocation(program, "diffuseProduct"),
+    flatten(diffuseProduct)
+  );
+  // wall # 1: in xz-plane
+  modelViewStack.push(modelViewMatrix);
+  modelViewMatrix = mult(modelViewMatrix, scale4(7, 1, 7));
+  DrawWall(0.02);
+  modelViewMatrix = modelViewStack.pop();
 
-  pointsArray.push(vertices[a]);
-  normalsArray.push(normal);
-  pointsArray.push(vertices[b]);
-  normalsArray.push(normal);
-  pointsArray.push(vertices[c]);
-  normalsArray.push(normal);
-  pointsArray.push(vertices[a]);
-  normalsArray.push(normal);
-  pointsArray.push(vertices[c]);
-  normalsArray.push(normal);
-  pointsArray.push(vertices[d]);
-  normalsArray.push(normal);
+  // wall #2: in yz-plane
+  modelViewStack.push(modelViewMatrix);
+  modelViewMatrix = mult(modelViewMatrix, rotate(90.0, 0.0, 0.0, 1.0));
+  modelViewMatrix = mult(modelViewMatrix, scale4(4, 1, 7));
+  DrawWall(0.02);
+  modelViewMatrix = modelViewStack.pop();
 
-  numVertices += 6;
+  // wall #3: in xy-plane
+  modelViewStack.push(modelViewMatrix);
+  modelViewMatrix = mult(modelViewMatrix, rotate(-90, 1.0, 0.0, 0.0));
+  modelViewMatrix = mult(modelViewMatrix, scale4(7, 1, 4));
+  DrawWall(0.02);
+  modelViewMatrix = modelViewStack.pop();
 }
 
 window.onload = function init() {
@@ -251,7 +396,7 @@ window.onload = function init() {
   }
 
   gl.viewport(0, 0, canvas.width, canvas.height);
-  gl.clearColor(1.0, 1.0, 1.0, 1.0);
+  gl.clearColor(0.7, 0.7, 0.7, 1.0);
 
   gl.enable(gl.DEPTH_TEST);
 
@@ -259,15 +404,16 @@ window.onload = function init() {
   program = initShaders(gl, "vertex-shader", "fragment-shader");
   gl.useProgram(program);
 
+  // generate points
   GenerateBowPoints();
 
-  // draw mesh items
+  // compute item points - the order in which they are computed matters
   QuadPresentBox();
-
   QuadPresentBow();
-
   QuadPresentWrap();
+  QuadPrism();
 
+  // #region setup
   // Buffers
   var nBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, nBuffer);
@@ -312,8 +458,9 @@ window.onload = function init() {
   );
 
   gl.uniform1f(gl.getUniformLocation(program, "shininess"), materialShininess);
+  // #endregion
 
-  // support user interface
+  // #region support user interface
   document.getElementById("phiPlus").onclick = function() {
     phi += deg;
   };
@@ -344,6 +491,7 @@ window.onload = function init() {
   document.getElementById("down").onclick = function() {
     translateFactorY -= 0.1;
   };
+  // #endregion
 
   render();
 };
@@ -376,9 +524,76 @@ var render = function() {
   gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
   gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
 
-  DrawPresentBox();
-  DrawPresentBow();
-  DrawPresentWrap();
+  // draw walls
+  DrawEnvironment();
+
+  // draw present
+  modelViewStack.push(modelViewMatrix);
+  modelViewMatrix = mult(modelViewMatrix, translate(5, 0.25, 1));
+  modelViewMatrix = mult(modelViewMatrix, scale4(0.5, 0.5, 0.5));
+  DrawPresent();
+  modelViewMatrix = modelViewStack.pop();
+
+  // draw house
+  modelViewStack.push(modelViewMatrix);
+  modelViewMatrix = mult(modelViewMatrix, translate(-0.4, 0, -1));
+  DrawHouse();
+  modelViewMatrix = modelViewStack.pop();
+
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
 
   requestAnimFrame(render);
 };
+
+/********** SUPPORTING FUNCTIONS **********/
+function scale4(a, b, c) {
+  var result = mat4();
+  result[0][0] = a;
+  result[1][1] = b;
+  result[2][2] = c;
+  return result;
+}
+
+function quad(a, b, c, d) {
+  var t1 = subtract(vertices[b], vertices[a]);
+  var t2 = subtract(vertices[c], vertices[b]);
+  var normal = cross(t1, t2);
+  var normal = vec3(normal);
+  normal = normalize(normal);
+
+  pointsArray.push(vertices[a]);
+  normalsArray.push(normal);
+  pointsArray.push(vertices[b]);
+  normalsArray.push(normal);
+  pointsArray.push(vertices[c]);
+  normalsArray.push(normal);
+  pointsArray.push(vertices[a]);
+  normalsArray.push(normal);
+  pointsArray.push(vertices[c]);
+  normalsArray.push(normal);
+  pointsArray.push(vertices[d]);
+  normalsArray.push(normal);
+
+  numVertices += 6;
+}
+
+function tri(a, b, c) {
+  var t1 = subtract(vertices[b], vertices[a]);
+  var t2 = subtract(vertices[c], vertices[b]);
+  var normal = cross(t1, t2);
+  var normal = vec3(normal);
+  normal = normalize(normal);
+
+  pointsArray.push(vertices[a]);
+  normalsArray.push(normal);
+  pointsArray.push(vertices[b]);
+  normalsArray.push(normal);
+  pointsArray.push(vertices[c]);
+  normalsArray.push(normal);
+  pointsArray.push(vertices[a]);
+  normalsArray.push(normal);
+  pointsArray.push(vertices[c]);
+  normalsArray.push(normal);
+
+  numVertices += 5;
+}
